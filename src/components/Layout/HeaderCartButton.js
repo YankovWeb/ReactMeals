acf0862eda12821db206ce3a9dd01ca2842598f8
Grigor Yankov <yankovweb@outlook.com>
2023-01-11 13:46:49 +0200

    [Future]Form

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CartIcon from "../Cart/CartIcon";
+import CartIcon from "../UI/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = () => {
   return (
