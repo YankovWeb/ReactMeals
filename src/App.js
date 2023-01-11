9ad26bfd3e22d512b09f180bd9d240207de07d1d
Grigor Yankov <yankovweb@outlook.com>
2023-01-11 14:45:29 +0200

    [Future]Modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import {useState} from "react";
 function App() {
+  const [cartIsShown, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+
   return (
     <>
-      <Header />
+      <Header onShowCart={showCartHandler} />
+      {cartIsShown ? <Cart onClose={hideCartHandler} /> : null}
       <main>
         <Meals />
       </main>
